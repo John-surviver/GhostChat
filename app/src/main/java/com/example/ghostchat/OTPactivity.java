3f39c1babf6455742b30230be059ecf3215b405c
johns-surviver <johnsurviver@gmail.com>
2022-01-18 23:26:56 +0600

    v1.01

--- app/src/main/java/com/example/ghostchat/OTPactivity.java
+++ app/src/main/java/com/example/ghostchat/OTPactivity.java
@@ -1,15 +1,57 @@
 package com.example.ghostchat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.ghostchat.databinding.ActivityOtpactivityBinding;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
 public class OTPactivity extends AppCompatActivity {
 
+    ActivityOtpactivityBinding binding;
+    FirebaseAuth mAuth;
+    String verificationId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_otpactivity);
+        binding=ActivityOtpactivityBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+        mAuth=FirebaseAuth.getInstance();
+        String phoneNumber = getIntent().getStringExtra("phonenumber");
+
+        binding.numbertxtid.setText("Verify "+ phoneNumber);
+
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(mAuth)
+                        .setPhoneNumber(phoneNumber)       // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this)                 // Activity (for callback binding)
+                        .setCallbacks(new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                            @Override
+                            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+
+                            }
+
+                            @Override
+                            public void onVerificationFailed(@NonNull FirebaseException e) {
+
+                            }
+
+                            @Override
+                            public void onCodeSent(@NonNull String verifyId, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                                super.onCodeSent(verifyId, forceResendingToken);
+                            }
+                        }).build();
+
+        PhoneAuthProvider.verifyPhoneNumber(options);
     }
 }
--- app/src/main/java/com/example/ghostchat/PhoneNumberVerification.java
+++ app/src/main/java/com/example/ghostchat/PhoneNumberVerification.java
@@ -2,21 +2,29 @@ package com.example.ghostchat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.ghostchat.databinding.ActivityPhoneNumberVerificationBinding;
 
 public class PhoneNumberVerification extends AppCompatActivity {
-
+    ActivityPhoneNumberVerificationBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_phone_number_verification);
-    }
 
+        binding=ActivityPhoneNumberVerificationBinding.inflate(getLayoutInflater());
 
-    void helloworld (){
+        setContentView(binding.getRoot());
 
-    }
-    void haha (){
-
+        binding.continueBtnid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PhoneNumberVerification.this,OTPactivity.class);
+                intent.putExtra("phonenumber",binding.inputNumberid.getText().toString());
+                startActivity(intent);
+            }
+        });
     }
 }
